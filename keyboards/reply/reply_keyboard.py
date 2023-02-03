58e30a9c11ce7926d3004440bef153028b07f52b
Igor Ponomarev <9129910001@mail.ru>
2023-02-03 22:59:11 +0500

    add no_city in eady_travel

--- keyboards/reply/reply_keyboard.py
+++
@@ -1,7 +0,0 @@
-# from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-# from loader import bot
-#
-#
-# def count_object():
-#     keyboard = ReplyKeyboardMarkup()
-#     num_1 = KeyboardButton(text='1', )
